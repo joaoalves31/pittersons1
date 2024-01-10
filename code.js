f0ad357d703afa3bb8c8aa327e64a5137fcee361
joaoalves31 <jvdsa@ufmg.br>
2024-01-10 16:22:48 -0300

    arrumações

--- code.js
+++ code.js
@@ -1,25 +1,24 @@
 function calcularRodadas() {
-    var apostaInicial = parseInt(document.getElementById('apostaInicial').value);
-    var saldoAtual = parseInt(document.getElementById('saldoAtual').value);
-    var multiplicador = parseFloat(document.getElementById('multiplicador').value);
-  
-    var rodada = 1;
-  
-    if ((multiplicador == 1.08 || multiplicador == 1.10) && apostaInicial * multiplicador < 12) {
-      var errorMessage = "A aposta inicial deve ser maior ou igual a " + Math.ceil(12 / multiplicador) + " para o multiplicador selecionado.";
-      document.getElementById('error').innerHTML = errorMessage;
-      return;
-    }
-  
-    document.getElementById('error').innerHTML = '';
-  
-    while (saldoAtual >= apostaInicial) {
-      saldoAtual -= apostaInicial;
-      apostaInicial = Math.ceil(apostaInicial * multiplicador);
-      rodada++;
-    }
-  
-    var resultadoElement = document.getElementById('resultado');
-    resultadoElement.innerHTML = "Quantidade de rodadas possíveis: " + (rodada - 1);
+  var apostaInicial = parseInt(document.getElementById('apostaInicial').value);
+  var saldoAtual = parseInt(document.getElementById('saldoAtual').value);
+  var multiplicador = parseFloat(document.getElementById('multiplicador').value);
+
+  var rodada = 1;
+
+  if ((multiplicador == 1.08 || multiplicador == 1.10) && apostaInicial * multiplicador < 12) {
+    var errorMessage = "A aposta inicial deve ser maior ou igual a " + Math.ceil(12 / multiplicador) + " para o multiplicador selecionado.";
+    document.getElementById('error').innerHTML = errorMessage;
+    return;
   }
-  
+
+  document.getElementById('error').innerHTML = ''; // Limpa a mensagem de erro
+
+  while (saldoAtual >= apostaInicial) {
+    saldoAtual -= apostaInicial;
+    apostaInicial = Math.ceil(apostaInicial * multiplicador);
+    rodada++;
+  }
+
+  var resultadoElement = document.getElementById('resultado');
+  resultadoElement.innerHTML = "Quantidade de apostas possíveis: " + (rodada - 1);
+}
