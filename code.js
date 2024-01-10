2c87c3b3e0ea46f178834133717f1a1eba484251
joaoalves31 <jvdsa@ufmg.br>
2024-01-10 15:59:07 -0300

    main

---
+++ code.js
@@ -0,0 +1,25 @@
+function calcularRodadas() {
+    var apostaInicial = parseInt(document.getElementById('apostaInicial').value);
+    var saldoAtual = parseInt(document.getElementById('saldoAtual').value);
+    var multiplicador = parseFloat(document.getElementById('multiplicador').value);
+  
+    var rodada = 1;
+  
+    if ((multiplicador == 1.08 || multiplicador == 1.10) && apostaInicial * multiplicador < 12) {
+      var errorMessage = "A aposta inicial deve ser maior ou igual a " + Math.ceil(12 / multiplicador) + " para o multiplicador selecionado.";
+      document.getElementById('error').innerHTML = errorMessage;
+      return;
+    }
+  
+    document.getElementById('error').innerHTML = '';
+  
+    while (saldoAtual >= apostaInicial) {
+      saldoAtual -= apostaInicial;
+      apostaInicial = Math.ceil(apostaInicial * multiplicador);
+      rodada++;
+    }
+  
+    var resultadoElement = document.getElementById('resultado');
+    resultadoElement.innerHTML = "Quantidade de rodadas possíveis: " + (rodada - 1);
+  }
+  
